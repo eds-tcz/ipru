d94171d1fe0ced91dc998442ccfcd9ea3af7a47b
eds-tcz <edstechchefz@gmail.com>
2024-11-04 15:14:04 +0530

    Footer Js

---
+++ blocks/ipru-footer/ipru-footer.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  [...block.children].forEach((row, r) => {
+    row.classList.add('footer-section');
+    if (r === 0) {
+      [...row.children].forEach((div, d) => {
+        // if (d === 0) {
+        div.classList.add('col-'.concat(d + 1));
+        const h3 = div.querySelector('h3');
+        div.querySelectorAll('h3').forEach((h3) => {
+          h3.classList.add('col-heading');
+        });
+        div.querySelectorAll('ul').forEach((ul) => {
+          ul.classList.add('col-list-lev-1');
+        });
+        div.querySelectorAll('ol').forEach((ol) => {
+          ol.classList.add('col-list-lev-2');
+        });
+      });
+    }
+  });
+}
