6580c7d6505cc2da5e179ab9c78c8d796a1ff2d0
eds-tcz <edstechchefz@gmail.com>
2024-11-12 06:23:39 +0530

    2 component js push

---
+++ blocks/ipru-promise/ipru-promise.js
@@ -0,0 +1,13 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.classList.add('ipru-prom-cards');
+    [...row.children].forEach((div, d) => {
+      if (d === 0) {
+        div.classList.add('prom-card-title');
+      }
+      if (d != 0) {
+        div.classList.add('prom-card-data', 'prom-data-'.concat(d));
+      }
+    });
+  });
+}
