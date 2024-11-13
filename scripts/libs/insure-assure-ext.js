eec3dbf9f86030d5ca54055fe01b1641a0ae5d94
eds-tcz <edstechchefz@gmail.com>
2024-11-13 05:36:42 +0530

    fix

--- scripts/libs/insure-assure-ext.js
+++ scripts/libs/insure-assure-ext.js
@@ -1,28 +1,28 @@
-let currentSlide = 0;
+// let currentSlide = 0;
 
-function showSlide(index) {
-  currentSlide = index;
-  updateCarousel();
-}
+// function showSlide(index) {
+//   currentSlide = index;
+//   updateCarousel();
+// }
 
-function nextSlide() {
-  currentSlide =
-    (currentSlide + 1) % document.querySelectorAll('.carousel-slide').length;
-  updateCarousel();
-}
+// function nextSlide() {
+//   currentSlide =
+//     (currentSlide + 1) % document.querySelectorAll('.carousel-slide').length;
+//   updateCarousel();
+// }
 
-function prevSlide() {
-  currentSlide =
-    (currentSlide - 1 + document.querySelectorAll('.carousel-slide').length) %
-    document.querySelectorAll('.carousel-slide').length;
-  updateCarousel();
-}
+// function prevSlide() {
+//   currentSlide =
+//     (currentSlide - 1 + document.querySelectorAll('.carousel-slide').length) %
+//     document.querySelectorAll('.carousel-slide').length;
+//   updateCarousel();
+// }
 
-function updateCarousel() {
-  document.querySelectorAll('.card').forEach((card, index) => {
-    card.classList.toggle('active', index === currentSlide);
-  });
-  document.querySelectorAll('.carousel-slide').forEach((slide, index) => {
-    slide.classList.toggle('active', index === currentSlide);
-  });
-}
+// function updateCarousel() {
+//   document.querySelectorAll('.card').forEach((card, index) => {
+//     card.classList.toggle('active', index === currentSlide);
+//   });
+//   document.querySelectorAll('.carousel-slide').forEach((slide, index) => {
+//     slide.classList.toggle('active', index === currentSlide);
+//   });
+// }
