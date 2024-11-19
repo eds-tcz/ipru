9c49d69b3062e62be2c40abcf37fadccbe1de39b
eds-tcz <edstechchefz@gmail.com>
2024-11-19 14:22:25 +0530

    bug fix

--- scripts/libs/insure-assure-ext.js
+++ scripts/libs/insure-assure-ext.js
@@ -1,28 +1,30 @@
-// let currentSlide = 0;
+function callFunction() {
+  let currentSlide = 0;
 
-// function showSlide(index) {
-//   currentSlide = index;
-//   updateCarousel();
-// }
+  window.updateCarousel = function () {
+    document.querySelectorAll('.card').forEach((card, index) => {
+      card.classList.toggle('active', index === currentSlide);
+    });
+    document.querySelectorAll('.carousel-slide').forEach((slide, index) => {
+      slide.classList.toggle('active', index === currentSlide);
+    });
+  };
 
-// function nextSlide() {
-//   currentSlide =
-//     (currentSlide + 1) % document.querySelectorAll('.carousel-slide').length;
-//   updateCarousel();
-// }
+  window.showSlide = function (index) {
+    currentSlide = index;
+    updateCarousel();
+  };
 
-// function prevSlide() {
-//   currentSlide =
-//     (currentSlide - 1 + document.querySelectorAll('.carousel-slide').length) %
-//     document.querySelectorAll('.carousel-slide').length;
-//   updateCarousel();
-// }
+  // prettier-ignore
+  window.nextSlide = function () {
+    currentSlide = (currentSlide + 1) % document.querySelectorAll('.carousel-slide').length;
+  updateCarousel();
+}
+  // prettier-ignore
+  window.prevSlide = function () {
+    currentSlide = (currentSlide - 1 + document.querySelectorAll('.carousel-slide').length) % document.querySelectorAll('.carousel-slide').length;
+  updateCarousel();
+}
+}
 
-// function updateCarousel() {
-//   document.querySelectorAll('.card').forEach((card, index) => {
-//     card.classList.toggle('active', index === currentSlide);
-//   });
-//   document.querySelectorAll('.carousel-slide').forEach((slide, index) => {
-//     slide.classList.toggle('active', index === currentSlide);
-//   });
-// }
+callFunction();
