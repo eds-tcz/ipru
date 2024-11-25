a6f01b985236abc2dcab2388f87aa7f041fd3cdc
eds-tcz <edstechchefz@gmail.com>
2024-11-25 10:52:26 +0530

    bug fix

--- blocks/ipru-features/ipru-features.js
+++ blocks/ipru-features/ipru-features.js
@@ -21,8 +21,8 @@ function loadExternalResources(resources) {
           }
 
           document.head.appendChild(element);
-        }),
-    ),
+        })
+    )
   );
 }
 export default function decorate(block) {
@@ -86,7 +86,7 @@ loadExternalResources([
           },
         });
       } catch (error) {
-        console.log(error.message);
+        // console.log(error.message);
       }
     }, 0);
   });
