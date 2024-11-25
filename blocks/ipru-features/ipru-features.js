83b01b58c492bfb972279b9b6c99bcef4c1562f6
eds-tcz <edstechchefz@gmail.com>
2024-11-25 13:09:36 +0530

    features fix

--- blocks/ipru-features/ipru-features.js
+++ blocks/ipru-features/ipru-features.js
@@ -28,28 +28,23 @@ function loadExternalResources(resources) {
 }
 export default function decorate(block) {
   [...block.children].forEach((row, r) => {
-    if (r === 0) {
-      row.classList.add('features-heading');
-    }
-    if (r !== 0) {
-      row.classList.add('feat-cards');
-      row.classList.add('feat-cards-'.concat(r));
+    row.classList.add('feat-cards');
+    row.classList.add('feat-cards-'.concat(r + 1));
 
-      [...row.children].forEach((div, d) => {
-        if (d === 0) {
-          div.classList.add('feat-table');
-        }
-        if (d === 1) {
-          div.classList.add('feat-body');
-        }
-        if (d === 2) {
-          div.classList.add('feat-line');
-        }
-        if (d === 3) {
-          div.classList.add('feat-cta');
-        }
-      });
-    }
+    [...row.children].forEach((div, d) => {
+      if (d === 0) {
+        div.classList.add('feat-table');
+      }
+      if (d === 1) {
+        div.classList.add('feat-body');
+      }
+      if (d === 2) {
+        div.classList.add('feat-line');
+      }
+      if (d === 3) {
+        div.classList.add('feat-cta');
+      }
+    });
   });
 }
 
